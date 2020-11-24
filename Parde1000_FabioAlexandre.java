b5ce52be0e42cada3f8d40fc4a73c5ed4da7dd3c
fabio alexandre <73499763+FabioAlexandre189@users.noreply.github.com>
2020-11-23 23:56:08 -0300

    Parde1000_FabioAlexandre

---
+++ Parde1000_FabioAlexandre.java
@@ -0,0 +1,16 @@
+
+package parde1000_fabioalexandre;
+
+
+public class Parde1000_FabioAlexandre {
+
+   
+    public static void main(String[] args) {
+       int numero=1000;
+    while (numero<1500)
+        { numero+=2;
+	System.out.println(numero);
+}
+    }
+    
+}
